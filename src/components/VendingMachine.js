447d7d21db473ecb447d78d9acd4c7546759243f
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-12-01 12:49:55 -0700

    removed presets; add main component outline

---
+++ src/components/VendingMachine.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VendingMachine = () => {
+    
+    return (
+      <div>
+
+        <div></div>
+            <h3>Options</h3>
+      </div>
+    );
+}
+
+export default VendingMachine;
+
