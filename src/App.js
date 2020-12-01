7d687d79d57170be23c2c34972ea59b3a5167c13
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-12-01 13:23:14 -0700

    home route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import "./App.css";
 import VendingMachine from './components/VendingMachine';
 
 function App() {
@@ -7,7 +10,13 @@ function App() {
       <header className="App-header">
         <h1>Vending Machine</h1>
       </header>
-      <VendingMachine />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <VendingMachine />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/VendingMachine.js
+++ src/components/VendingMachine.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const VendingMachine = () => {
     
     return (
       <div>
-
-        <div></div>
-            <h3>Options</h3>
+        <h3>Options</h3>
+        
       </div>
     );
 }
